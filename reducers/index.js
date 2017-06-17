efd765e9339642d49bd3e3defbaf10045fdcb896
ischoe <schoenberger.ingo@gmail.com>
2017-06-17 13:37:20 +0200

    remove files

--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import reactSelectSuggest from '../components/ReactSelectSuggest/ReactSelectSuggestReducer';
-
-const rootReducer = combineReducers({
-    reactSelectSuggest
-});
-
-export default rootReducer
